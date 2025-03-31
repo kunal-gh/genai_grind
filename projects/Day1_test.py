34e9e950086874442a9abfeb80ae555c28ce482f
kunal-gh <2112sainikunal@gmail.com>
2025-03-31 10:28:55 +0530

    Day1:{whatever}

--- projects/Day1_test.py
+++ projects/Day1_test.py
@@ -1,1 +1,7 @@
 print("hello world")
+def greet(name):
+    """Generate a greeting using AI-style formatting"""
+    return f"Hello {name}! >Initializing gen AI journey...."
+if __name__ == "__main__":
+    user = input("Enter you name: ")
+    print(greet)
